f14e94731f008bd65b684b49f6d921d38107d196
rasagiX <aggrashi974@gmail.com>
2026-03-04 00:09:06 +0530

    Section 3

--- Aivionix/src/App.jsx
+++ Aivionix/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Section1 from './component/section1/section1.jsx'
 import Section2 from './component/section2/section2.jsx'
+import Section3 from './component/section3/section3.jsx'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Section1/>
       <Section2/>
+      <Section3/>
     </div>
   )
 }
